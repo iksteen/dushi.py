652afb3876f8bd186cd74b6f3aacbf22accafd70
Natte Wasbeer <whollah@wasbeer.nl>
2013-08-18 00:50:00 +0200

    Eerste commit G

---
+++ README.md
@@ -0,0 +1,18 @@
+dushi.py
+========
+
+bam
+
+Instalatie
+===================================================
+Ewa nifa op deze tan2lauwe dushi.py bestandjuh gwn ff chmod +x zette en met die irssi kk je m gwn in ~/.irssi jatok niet dan
+
+Gebruik
+===================================================
+br0 gwn ./dushi.py dan je berigt in terminaal tiepe dan kryg je m tan2vers trug tok
+
+Irssi
+===================================================
+Ait brada jy gebruikt irssi goed bezig tog ja tog G btr gebruik je dese alias swa
+
+/alias dushi /exec - -o ~/.irssi/dushi.py
---
+++ dushi.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+__author__ = "dsc @ #2600nl @ irc.smurfnet.ch"
+__copyright__ = "nope.jpg"
+__credits__ = ["Wazakindjes, rawplayer"]  # 1 lobbi!1
+__maintainer__ = "Wazakindjes, rawplayer"
+__version__ = "17 aug 2013"
+from random import randrange
+import re
+import sys
+
+if len(sys.argv) <= 1:
+    print 'gast, input?\n'
+    sys.exit()
+
+class Whollah():
+    def __init__(self):
+        self.bezem = {}
+
+        def parse_dict():
+            f = open('sagbi.txt')
+            a = f.readlines()
+            f.close()
+            for l in a:
+                l = l.replace('\n', '').split('=')
+                key = l[0]
+                value = l[1].split(',')
+                self.bezem[key] = value
+
+        parse_dict()
+
+    # ewa
+    def sagbi(self, a):
+        for k, v in self.bezem.items():
+            for i in k.split(','):
+                if i == a:
+                    # bam, gevonden die shit
+                    return v
+            # skeere input
+        return None
+
+    def haxor(self, a):
+        def bingo():
+            # :OOOOOO
+            if randrange(0, 6) == 3:
+                # BAM
+                return True
+            # :(((((((((
+            return False
+
+        if bingo():
+            a = a.replace('e', '3')
+        if bingo():
+            a = a.replace('a', '4')
+        if bingo():
+            a = a.replace('o', '0')
+        if bingo():
+            a = a.replace('i', '1')
+
+        # BAM, ZONNE GROTE KNAL
+        return ''.join([''.join([x.upper() if bingo() else x]) for x in list(a)])
+
+
+if __name__ == "__main__":
+    # werd up
+    poepie = Whollah()
+
+    # waz met deze
+    zemmel = ' '.join(sys.argv[1::])
+
+    out = []
+    for w in zemmel.split():
+
+        # zoek in die ding G
+        sletje = poepie.sagbi(re.sub(r'\W+', '', w).lower())
+
+        if sletje:
+            # BAM
+            new = sletje[randrange(0, len(sletje))]
+
+            # ff die kommas en punten terughalen als ze er waren :@@@@@
+            new = [new + '.' if w.endswith('.') else new + ',' if w.endswith(',') else new][0]
+
+            # heuelemaal mooi
+            out.append(new)
+        else:
+            # skeer
+            out.append(w)
+
+    # BAM KLAAR OUTPUT ALLES
+    breezah = ' '.join(out)
+
+    # ok, nog ff hax0rfyen
+    breezah = poepie.haxor(breezah)
+
+    # BAM KLAAR
+    print breezah
