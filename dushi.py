288e3fff873eb5b76790495e8f1d63ed9c1920cd
Natte Wasbeer <whollah@wasbeer.nl>
2013-08-18 18:30:55 +0200

    dit dat

--- dushi.py
+++ dushi.py
@@ -10,11 +10,12 @@ import re
 import sys
 import os
 
-FILE_DICT = 'sagbi.txt'
+FILE_DICT = 'dushi.db'
 PATH_DICT = '%s%s%s' % ('/'.join(os.path.abspath(__file__).split('/')[:-1]),'/',FILE_DICT) # wat
 
-DUSHI_GEHALTE = 6 # hoe lager, hoe meer dushi. minimaal 3.
-	
+DUSHI_GEHALTE = 6  # hoe lager, hoe meer dushi. minimaal 3.
+
+
 class Whollah():
     global PATH_DICT
 
@@ -63,58 +64,58 @@ class Whollah():
 
 if __name__ == "__main__":
 
-	p = argparse.ArgumentParser()
-	p.add_argument('--update', help='updeet db G', action='store_true')
-	p.add_argument('--halp', help='probelm?', action='store_true')
-	args, zemmel = p.parse_known_args()
+    p = argparse.ArgumentParser()
+    p.add_argument('--update', help='updeet db G', action='store_true')
+    p.add_argument('--halp', help='probelm?', action='store_true')
+    args, zemmel = p.parse_known_args()
 
-	if len(zemmel) == 1:
-		zemmel = ''.join(zemmel).split() # args fix
-			
-	if args.update:
-		import urllib2
-		db = 'https://raw.github.com/nattewasbeer/dushi.py/master/sagbi.txt'
-		
-		response = urllib2.urlopen(db)
-		f = open(PATH_DICT,'w')
-		f.write(response.read())
-		f.close()
-	
-		print '%s up2date, bam' % FILE_DICT
+    if len(zemmel) == 1:
+        zemmel = ''.join(zemmel).split() # args fix
 
-	elif args.halp:
-		print 'https://github.com/nattewasbeer/dushi.py/blob/master/README.md'
+    if args.update:
+        import urllib2
+        db = 'https://raw.github.com/nattewasbeer/dushi.py/master/sagbi.txt'
 
-	elif zemmel:
-	    # werd up
-	    skeere = Whollah()
+        response = urllib2.urlopen(db)
+        f = open(PATH_DICT,'w')
+        f.write(response.read())
+        f.close()
 
-	    dushi = []
-	    for w in zemmel:
+        print '%s up2date, bam' % FILE_DICT
 
-		# zoek ding
-		chimeid = skeere.sagbi(re.sub(r'\W+', '', w).lower())
+    elif args.halp:
+        print 'https://github.com/nattewasbeer/dushi.py/blob/master/README.md'
 
-		if chimeid: # jwt
-		    new = chimeid[randrange(0, len(chimeid))]
+    elif zemmel:
+        # werd up
+        skeere = Whollah()
 
-		    # kommas, punten, how do they werk!?
-		    new = new + '.' if w.endswith('.') else new + ',' if w.endswith(',') else new
+        dushi = []
+        for w in zemmel:
 
-		    # heuelemaal mooi
-		    dushi.append(new)
-		else:
-		    # skeer
-		    dushi.append(w)
+            # zoek ding
+            chimeid = skeere.sagbi(re.sub(r'\W+', '', w).lower())
 
-	    # EINDELIJK KLAAR
-	    deze = ' '.join(dushi)
+            if chimeid: # jwt
+                new = chimeid[randrange(0, len(chimeid))]
 
-	    # deze = skeere.haxor(deze), jwt
-	    deze = skeere.haxor(deze)
+                # kommas, punten, how do they werk!?
+                new = new + '.' if w.endswith('.') else new + ',' if w.endswith(',') else new
 
-	    # BAM
-	    print deze
-	else:
-		# gast, input?
-		pass
+                # heuelemaal mooi
+                dushi.append(new)
+            else:
+                # skeer
+                dushi.append(w)
+
+        # EINDELIJK KLAAR
+        deze = ' '.join(dushi)
+
+        # deze = skeere.haxor(deze), jwt
+        deze = skeere.haxor(deze)
+
+        # BAM
+        print deze
+    else:
+        # gast, input?
+        pass
